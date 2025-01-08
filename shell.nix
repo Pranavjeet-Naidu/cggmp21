03db6491e63c3205df25d5d3d32b7e82144f6431
Denis Varlakov <denis@dfns.co>
2025-01-08 17:39:26 +0100

    Update DKGs
    Signed-off-by: Denis Varlakov <denis@dfns.co>

--- shell.nix
+++ shell.nix
@@ -1,16 +1,9 @@
-let # Rust
-  pkgs = import <nixpkgs> { overlays = [ rustOverlay ]; };
-  lib = pkgs.lib;
+# shell for compiling latex spec
+
+let
+  pkgs = import <nixpkgs> {};
   isDarwin = pkgs.hostPlatform.isDarwin;
 
-  rustVersion = "1.75.0";
-  rustOverlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
-
-  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
-    extensions = [
-      "rust-src" # for rust-analyzer
-    ];
-  };
   # Latex
   tex = (pkgs.texlive.combine {
     inherit (pkgs.texlive) scheme-small
@@ -20,7 +13,7 @@ let # Rust
 in pkgs.stdenv.mkDerivation {
   name = "signers-env";
   nativeBuildInputs = [
-    rust pkgs.rust-analyzer tex pkgs.gnum4
+    tex
   ];
-  buildInputs = lib.optionals isDarwin [pkgs.darwin.apple_sdk.frameworks.Security];
+  buildInputs = pkgs.lib.optionals isDarwin [pkgs.darwin.apple_sdk.frameworks.Security];
 }
